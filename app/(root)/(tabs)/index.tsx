f23aaa69bd1d1ea8cab454eabad68a8c562ab643
winqus <67459222+winqus@users.noreply.github.com>
2025-02-17 18:46:10 +0200

    Add setup for expo routes

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+import {Link} from "expo-router";
+
+export default function Index() {
+    return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+        <Link href="/sign-in">Sign In</Link>
+        <Link href="/explore">Explore</Link>
+        <Link href="/profile">Profile</Link>
+        <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
