a1a3d3281294cd90f0e8e2a6e35b34401367d69a
winqus <67459222+winqus@users.noreply.github.com>
2025-02-19 18:11:13 +0200

    Add functional auth with sign-in and global context. Add global-provider.tsx and useAppwrite.ts. Rename getUser() to getCurrentUser(). Update layouts and sign-in.tsx.

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -67,7 +67,7 @@ export async function logout() {
   }
 }
 
-export async function getUser() {
+export async function getCurrentUser() {
   try {
     const response = await account.get();
 
---
+++ lib/global-provider.tsx
@@ -0,0 +1,59 @@
+import { createContext, ReactNode, useContext } from "react";
+import { useAppwrite } from "@/lib/useAppwrite";
+import { getCurrentUser } from "./appwrite";
+
+interface User {
+  $id: string;
+  name: string;
+  email: string;
+  avatar: string;
+}
+
+interface GlobalContextType {
+  isLoggedIn: boolean;
+  user: User | null;
+  loading: boolean;
+  refetch: (newParams: Record<string, string | number>) => Promise<void>;
+}
+
+const GlobalContext = createContext<GlobalContextType | undefined>(undefined);
+
+interface GlobalProviderProps {
+  children: ReactNode;
+}
+
+export const GlobalProvider = ({ children }: GlobalProviderProps) => {
+  const {
+    data: user,
+    loading,
+    refetch,
+  } = useAppwrite({
+    fn: getCurrentUser,
+  });
+
+  const isLoggedIn = !!user;
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        isLoggedIn,
+        user,
+        loading,
+        refetch,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = (): GlobalContextType => {
+  const context = useContext(GlobalContext);
+  if (!context) {
+    throw new Error("useGlobalContext must be used within a GlobalProvider");
+  }
+
+  return context;
+};
+
+export default GlobalProvider;
