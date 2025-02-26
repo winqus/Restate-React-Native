f724a2da96ec6f25c46357be604dc7e545708c29
winqus <67459222+winqus@users.noreply.github.com>
2025-02-26 10:53:54 +0200

    Add property Details screen

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -159,3 +159,18 @@ export async function getProperties({
     return [];
   }
 }
+
+export async function getPropertyById({ id }: { id: string }) {
+  try {
+    const result = await databases.getDocument(
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      id,
+    );
+
+    return result;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
